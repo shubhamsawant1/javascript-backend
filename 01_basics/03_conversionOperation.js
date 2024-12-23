71050bbe187ac4a782b4c926f51459048f51b54d
shubhamsawant1 <shubhamsawant22011@gmail.com>
2024-12-23 19:13:18 +0000

    conversition of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// True => 1; false => 0
+// null => 0
+// undefined =>
+
+    let isLoggedIn = "shubham"
+
+    let booleanIsLoggedIn = Boolean(isLoggedIn)
+    console.log(booleanIsLoggedIn);
+
+    // 1 => true; 0 => false
+    // "" => false
+    // "shubham" => true
+
+
+    let someNumber = 33
+    
+    let stringNumber = String(someNumber)
+    console.log(stringNumber);
+    console.log(typeof stringNumber);
+
+    
+
