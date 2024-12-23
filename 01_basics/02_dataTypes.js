1360389d5bf18cfc10399260488e70b0e5bd378a
shubhamsawant1 <shubhamsawant22011@gmail.com>
2024-12-23 18:24:59 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //true all js code as newer version
+
+//alert(3 + 3) // we are using nodejs ,not browser
+
+console.log(3
+    + 
+    3) // code readability should be high
+
+let name = "shubham"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => stand Alone value
+// undefined =>
+// symbol => uniques
+
+// object
+
+console.log(typeof "shubaham");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
+
