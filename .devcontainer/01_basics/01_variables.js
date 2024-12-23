26490254a55a317020ca1d6668f57a6f5fc19a86
shubhamsawant1 <shubhamsawant22011@gmail.com>
2024-12-23 18:24:12 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 155486
+let accountEmail = "shubhamsawant@gmail.com"
+var accountPasseord = "12364"
+accountCity = "jaipur"
+
+account = 2
+
+console.log(accountTd);
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmails = "shubham@gmail.com"
+var accountPasswor = "113365"
+accountCity = "kolhapur"
+let accountState;
+
+// account = 2 // not allowed
+
+accountEmail = "gonuse@gmail.com"
+accountPassword = "256418"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
